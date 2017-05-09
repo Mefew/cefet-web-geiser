ec5c8e7ce5eb458ba1e7403e18204d7779f8c0e2
Aluno <aluno@decom-OptiPlex-780>
2017-05-09 12:27:02 -0300

    Inicial

--- server/app.js
+++ server/app.js
@@ -1,16 +1,24 @@
 var express = require('express'),
     app = express();
+var fs = require ('fs');
 
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
 // você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
 // dica: 3-4 linhas de código (você deve usar o módulo de filesystem (fs))
-var db = {
-};
-
+var db = {};
+var dir = '/home/aluno/Área de Trabalho/Mefew/cefet-web-geiser/server';
+fs.readFile(dir+'/data/jogadores.json', 'utf8', function (err, data) {
+	if (err) throw err;
+  	db["jogadores"] = JSON.parse(data);
+});
+fs.readFile(dir+'/data/jogosPorJogador.json', 'utf8', function (err, data) {
+	if (err) throw err;
+  	db["jogosPorJogador"] = JSON.parse(data);
+});
 
 // configurar qual templating engine usar. Sugestão: hbs (handlebars)
-//app.set('view engine', '???');
-
+app.set('view engine', 'hbs');
+app.set('views', './views')
 
 // EXERCÍCIO 2
 // definir rota para página inicial --> renderizar a view index, usando os
@@ -28,6 +36,10 @@ var db = {
 // EXERCÍCIO 1
 // configurar para servir os arquivos estáticos da pasta "client"
 // dica: 1 linha de código
+app.use(express.static('client'));
 
 // abrir servidor na porta 3000
 // dica: 1-3 linhas de código
+app.listen(3000, function () {
+	console.log ("Server listening at http://127.0.0.1:3000");
+});
